8b8892ff54f58e1a2dde42e7be0dbae8ced7e83f
nhuyk56@gmail.com <y.nguyen@9thwonder.com>
2022-01-03 17:21:19 +0700

    ac-ma-ngay-o-ben-nguoi

---
+++ ac-ma-ngay-o-ben-nguoi/ac-ma-ngay-o-ben-nguoi/pre/102900-tap-91-chuong-2912---chuong-2943.ps1
@@ -0,0 +1,47 @@
+﻿$vidDef = "D:\lib-video\924.mp4"
+
+$truyenSlug = "ac-ma-ngay-o-ben-nguoi"
+$truyenName = "Ác Ma Ngay Ở Bên Người"
+$truyenPngUrl = "https://flamingtext.com/net-fu/image_output.cgi?_comBuyRedirect=false&script=smurfs-logo&text=%C3%81c%0AMa%0ANgay%0A%E1%BB%9E%0AB%C3%AAn%0ANg%C6%B0%E1%BB%9Di&symbol_tagname=popular&fontsize=200&fontname=bubblegum%20sans&fontname_tagname=cool&textBorder=5&growSize=0&antialias=on&hinting=on&justify=2&letterSpacing=0&lineSpacing=0&textSlant=0&textVerticalSlant=0&textAngle=0&textOutline=off&textOutline=false&textOutlineSize=2&fillTextType=2&fillTextColor=%237F7F7F&fillTextPattern=Wood&fillTextPattern_tagname=standard&fillTextGradient=Blue%20Green&fillTextGradient_tagname=standard&fillTextGradientDirection=0&fillTextGradientReverse=off&fillTextGradientSmartFit=0&outlineSize=4&fillOutlineType=0&fillOutlineColor=%23169CCC&fillOutlinePattern=Wood&fillOutlinePattern_tagname=standard&fillOutlineGradient=Full%20saturation%20spectrum%20CCW&fillOutlineGradient_tagname=standard&fillOutlineGradientDirection=0&fillOutlineGradientReverse=off&fillOutlineGradientSmartFit=0&outline2Size=3&fillOutline2Type=0&fillOutline2Color=%23050A36&fillOutline2Pattern=Wood&fillOutline2Pattern_tagname=standard&fillOutline2Gradient=Full%20saturation%20spectrum%20CCW&fillOutline2Gradient_tagname=standard&fillOutline2GradientDirection=0&fillOutline2GradientReverse=off&fillOutline2GradientSmartFit=0&curveHighlight=off&curveHighlight=false&curveBrightness=90&curveFactor=1.5&curveOffsetX=0&curveOffsetY=5&shadowOnFirstOutline=off&shadowType=1&shadowXOffset=4&shadowYOffset=4&shadowBlur=4&shadowColor=%23050A36&shadowOpacity=40&reflectOpacity=50&reflectTiltX=0&reflectPercent=66&reflectScaleYPercent=100&reflectXOffset=0&reflectYOffset=0&shadowGlowColor=%230000FF&shadowGlowSize=2&shadowGlowFeather=5&shadowNormalColor=%233C3C3C&shadowNormalFeather=2&shadowNormalOpacity=50&shadowNormalTiltX=40&shadowNormalScaleYPercent=65&shadowNormalXOffset=0&shadowNormalYOffset=0&shadowSelfXOffset=10&shadowSelfYOffset=10&shadowSelfBlur=0&shadowSelfOpacity=50&backgroundResizeToLayers=on&backgroundRadio=0&backgroundColor=%23FFFFFF&backgroundPattern=Wood&backgroundPattern_tagname=standard&backgroundGradient=Full%20saturation%20spectrum%20CCW&backgroundGradient_tagname=standard&backgroundStarburstColorAlt=%23ED2400&backgroundStarburstColor1=%23BD2409&backgroundStarburstNum=25&backgroundStarburstRayPercentage=50&backgroundStarburstUseColor2=on&backgroundStarburstUseColor2=false&backgroundStarburstColor2=%23000000&backgroundStarburstOffsetAngle=0&backgroundStarburstXOffset=0&backgroundStarburstYOffset=0&backgroundStarburstCenterPercentage=2&backgroundStarburstRadiusX=300&backgroundStarburstRadiusY=300&backgroundUseOverlay=off&backgroundOverlayMode=5&backgroundOverlayPattern=Parque%20%231&backgroundOverlayPattern_tagname=standard&backgroundOverlayOpacity=100&backgroundImageUrl=http%3A%2F%2Fwww.flamingtext.com%2Fimages%2Ftextures%2Ftexture3.jpg&useFixedSize=off&useFixedSize=false&imageWidth=400&imageHeight=150&imageAlignment=4&autocrop=off&autocrop=false&autocropPadding=0&watermark=none&ext=png&jpgQuality=85&doScale=off&scaleWidth=240&scaleHeight=120"
+
+$tapIndex = 91
+$tapSlug = "tap-91-chuong-2912---chuong-2943"
+$TapName = "Tập 91. Chương 2912 -> Chương 2943"
+$tapTxtUrl = "D:\SyncAudioStorage\GroupsTxt\b885d09d715d6c7c760c4309d8ab8dcf.txt"
+$tapPngUrl = "https://flamingtext.com/net-fu/image_output.cgi?_comBuyRedirect=false&script=smurfs-logo&text=T%E1%BA%ADp%2091&symbol_tagname=popular&fontsize=200&fontname=bubblegum%20sans&fontname_tagname=cool&textBorder=5&growSize=0&antialias=on&hinting=on&justify=2&letterSpacing=0&lineSpacing=0&textSlant=0&textVerticalSlant=0&textAngle=0&textOutline=off&textOutline=false&textOutlineSize=2&fillTextType=2&fillTextColor=%237F7F7F&fillTextPattern=Wood&fillTextPattern_tagname=standard&fillTextGradient=Blue%20Green&fillTextGradient_tagname=standard&fillTextGradientDirection=0&fillTextGradientReverse=off&fillTextGradientSmartFit=0&outlineSize=4&fillOutlineType=0&fillOutlineColor=%23169CCC&fillOutlinePattern=Wood&fillOutlinePattern_tagname=standard&fillOutlineGradient=Full%20saturation%20spectrum%20CCW&fillOutlineGradient_tagname=standard&fillOutlineGradientDirection=0&fillOutlineGradientReverse=off&fillOutlineGradientSmartFit=0&outline2Size=3&fillOutline2Type=0&fillOutline2Color=%23050A36&fillOutline2Pattern=Wood&fillOutline2Pattern_tagname=standard&fillOutline2Gradient=Full%20saturation%20spectrum%20CCW&fillOutline2Gradient_tagname=standard&fillOutline2GradientDirection=0&fillOutline2GradientReverse=off&fillOutline2GradientSmartFit=0&curveHighlight=off&curveHighlight=false&curveBrightness=90&curveFactor=1.5&curveOffsetX=0&curveOffsetY=5&shadowOnFirstOutline=off&shadowType=1&shadowXOffset=4&shadowYOffset=4&shadowBlur=4&shadowColor=%23050A36&shadowOpacity=40&reflectOpacity=50&reflectTiltX=0&reflectPercent=66&reflectScaleYPercent=100&reflectXOffset=0&reflectYOffset=0&shadowGlowColor=%230000FF&shadowGlowSize=2&shadowGlowFeather=5&shadowNormalColor=%233C3C3C&shadowNormalFeather=2&shadowNormalOpacity=50&shadowNormalTiltX=40&shadowNormalScaleYPercent=65&shadowNormalXOffset=0&shadowNormalYOffset=0&shadowSelfXOffset=10&shadowSelfYOffset=10&shadowSelfBlur=0&shadowSelfOpacity=50&backgroundResizeToLayers=on&backgroundRadio=0&backgroundColor=%23FFFFFF&backgroundPattern=Wood&backgroundPattern_tagname=standard&backgroundGradient=Full%20saturation%20spectrum%20CCW&backgroundGradient_tagname=standard&backgroundStarburstColorAlt=%23ED2400&backgroundStarburstColor1=%23BD2409&backgroundStarburstNum=25&backgroundStarburstRayPercentage=50&backgroundStarburstUseColor2=on&backgroundStarburstUseColor2=false&backgroundStarburstColor2=%23000000&backgroundStarburstOffsetAngle=0&backgroundStarburstXOffset=0&backgroundStarburstYOffset=0&backgroundStarburstCenterPercentage=2&backgroundStarburstRadiusX=300&backgroundStarburstRadiusY=300&backgroundUseOverlay=off&backgroundOverlayMode=5&backgroundOverlayPattern=Parque%20%231&backgroundOverlayPattern_tagname=standard&backgroundOverlayOpacity=100&backgroundImageUrl=http%3A%2F%2Fwww.flamingtext.com%2Fimages%2Ftextures%2Ftexture3.jpg&useFixedSize=off&useFixedSize=false&imageWidth=400&imageHeight=150&imageAlignment=4&autocrop=off&autocrop=false&autocropPadding=0&watermark=none&ext=png&jpgQuality=85&doScale=off&scaleWidth=240&scaleHeight=120"
+
+$temp = "D:\Temp"
+$truyenPngName = "$($temp)/$($truyenSlug)-TruyenName-$($tapIndex).png";
+$truyenPngRSName = "$($temp)/$($truyenSlug)-TruyenName-$($tapIndex)-rs.png";
+
+$tapPngName     = "$($temp)/$($truyenSlug)-TapName-$($tapIndex).png";
+$tapPngRSName   = "$($temp)/$($truyenSlug)-TapName-$($tapIndex)-rs.png";
+
+$tapTxtName     = "$($temp)/$($truyenSlug)-TapName-$($tapIndex).txt";
+$tapOggName     = "$($temp)/$($truyenSlug)-TapName-$($tapIndex).ogg";
+$tapMp3Name     = "$($temp)/$($truyenSlug)-TapName-$($tapIndex).mp3";
+$tapMp4Name     = "$($truyenSlug)-$($tapSlug).mp4";
+
+$transparentPngUrl = "http://t1.daumcdn.net/brunch/service/guest/file/PcW-acZJGNuyl6YOic1gZzImj2A.png"
+$transparentPngName = "$($temp)/$($truyenSlug)-TruyenName-$($tapIndex)-transparent.png"
+
+$circleLogoPngUrl = "https://i.imgur.com/v6vwPR2.png"
+$circleLogoPngName = "$($temp)/$($truyenSlug)-TruyenName-$($tapIndex)-circle-logo.png"
+
+$F1 = "$($temp)/$($truyenSlug)-TapName-$($tapIndex)-F1.png";
+$F2 = "$($temp)/$($truyenSlug)-TapName-$($tapIndex)-F2.png";
+$F3 = "$($temp)/$($truyenSlug)-TapName-$($tapIndex)-F3.png";
+$F4 = "$($temp)/$($truyenSlug)-TapName-$($tapIndex)-F4.mp4";
+
+$qc = "Phát hành trên kênh nằm nghe truyện chấm com. Linh trọn bộ dưới phần mô tả."
+
+echo "-----------------------START-----------------------";
+$truyenNameObj = Invoke-RestMethod -Method Get -Uri $truyenPngUrl
+$tap01Obj = Invoke-RestMethod -Method Get -Uri $tapPngUrl
+
+wget $truyenNameObj.src -o $truyenPngName
+wget $tap01Obj.src -o $tapPngName
+wget $tapTxtUrl -o $tapTxtName
+wget $transparentPngUrl -o $transparentPngName
+wget $circleLogoPngUrl -o $circleLogoPngName
+pause;
