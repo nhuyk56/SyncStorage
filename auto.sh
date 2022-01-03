815cd427712fa543e8be86fbd66ade59482800fb
nhuyk56@gmail.com <y.nguyen@9thwonder.com>
2022-01-03 18:56:36 +0700

    a

--- auto.sh
+++ auto.sh
@@ -1,10 +1,27 @@
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/nhuyk56
-ssh -T git@github.com
-git checkout main
-git checkout -b $1
-cp -r D:/YOUTUBE-NNT/$1 $1
-git add $1
-git commit -m $1
-git push -u origin $1
-git checkout main
+if [ -d D:/YOUTUBE-NNT/$1 ] 
+then
+  echo 1
+  eval "$(ssh-agent -s)"
+  echo 2
+  ssh-add ~/.ssh/nhuyk56
+  echo 3
+  ssh -T git@github.com
+  echo 4
+  git checkout main
+  echo 5
+  git checkout -b $1
+  echo 6
+  cp -r D:/YOUTUBE-NNT/$1 $1
+  echo 7
+  git add $1
+  echo 8
+  git commit -m $1
+  echo 9
+  git push -u origin $1
+  echo 10
+  git checkout main
+  echo 11
+else
+  echo "Error: Directory /path/to/dir does not exists."
+fi
+read -p "Pause Time 5 seconds"
