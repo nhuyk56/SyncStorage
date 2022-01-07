0d1a78f0c571b50c0730c6ab52ad42442187dc89
nhuyk56 <nhuyk56@gmail.com>
2022-01-07 13:51:14 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var { uploadToGit } = require('./uploadToGit.js');
 const u2g = () => {
-  const pathFileName = 'D:/YOUTUBE-NNT/0YOUUTBE-bat-dau-ban-thuong-bay-cai-the-nhan-vat/107727-tap-26-chuong-598---chuong-624.ps1'
-  const slugFileName = '123-test'
+  const pathFileName = 'D:/SyncAudioStorage/GroupsTxt/3fb8f41fd5409f6a94013eea51debbf0.txt'
+  const slugFileName = 'warning1.txt'
   const envFolder = 'C:/Users/YNN/Downloads'
-  const gitSource = 'https://github.com/nhuyk56/SyncStorage.git'
+  const gitSource = 'git@github.com:nhuyk56/SyncStorage1.git'
   const data = uploadToGit({
     envFolder,
     gitSource,
