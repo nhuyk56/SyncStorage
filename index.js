2d1fc30b3e80df65cdc56bbdf9c171a5c631d117
nhuyk56 <nhuyk56@gmail.com>
2022-01-11 07:38:39 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var { uploadToGit, mp3ToHls } = require('./uploadToGit.js');
 const u2g = () => {
   const pathFileName = 'C:/Users/YNN/Desktop/SyncStorage/case/dai-nguy-doc-sach-nguoi-tap-1.mp3'
-  const slugFileName = 'dai-nguy-doc-sach-nguoi-tap-2'
+  const slugFileName = 'dai-nguy-doc-sach-nguoi-tap-4'
   const envFolder = 'C:/Users/YNN/Downloads'
-  const gitSource = 'git@github.com:nhuyk56/SyncStorage1.git'
+  const gitSource = 'git@alias-git-storage:nhuyk56/SyncStorage1.git'
   const data = uploadToGit({
     envFolder,
     gitSource,
--- uploadToGit.js
+++ uploadToGit.js
@@ -10,6 +10,12 @@ const getFileName = (inputPath) => inputPath.split('/').pop()
 
 const uploadToGit= ({ envFolder, gitSource, pathFileName, slugFileName }) => {
   var gitPath = gitSource.replace('.git', '').replace('git@github.com:', 'https://github.com/')
+  if (gitSource.includes('git@')) {
+    const host = 'https://github.com/'
+    const gitId = gitSource.split(':').pop() // [git@alias-git-storage]:[nhuyk56/SyncStorage1.git] > nhuyk56/SyncStorage1.git
+      .replace('.git', '') // nhuyk56/SyncStorage1
+    gitPath = host + gitId
+  }
   var gitFolder = gitPath.split('/').pop()
   var fs = require('fs');
   var path = require('path');
@@ -17,8 +23,6 @@ const uploadToGit= ({ envFolder, gitSource, pathFileName, slugFileName }) => {
   var md5 = require('md5');
 
   const shellOption = { cwd: envFolder, shell: 'C:/Program Files/Git/bin/sh.exe', windowsHide: true, silent: false }
-  // const pathFileName = 'D:/YOUTUBE-NNT/0YOUUTBE-bat-dau-ban-thuong-bay-cai-the-nhan-vat/107727-tap-26-chuong-598---chuong-624.ps1'
-  // const slugFileName = '107727-tap-26-chuong-598---chuong-624'
   const brandMD5 = md5(slugFileName)
 
   var res = { status: {}, code: false }
@@ -37,11 +41,7 @@ const uploadToGit= ({ envFolder, gitSource, pathFileName, slugFileName }) => {
     res.isFile = fs.lstatSync(pathFileName).isFile()
   }
   shell.exec(`rm -rf ${gitFolder}`, shellOption)
-  const isGitClone = shell.exec(`
-                          eval "$(ssh-agent -s)"
-                          ssh-add ~/.ssh/nhuyk56
-                          ssh -T git@github.com
-                          git clone ${gitSource}`, shellOption).code === 0
+  const isGitClone = shell.exec(`git clone ${gitSource}`, shellOption).code === 0
   setStatus(isGitClone, 'isGitClone')
   if (isGitClone) {
     shellOption.cwd = path.join(envFolder, gitFolder)
@@ -71,13 +71,7 @@ const uploadToGit= ({ envFolder, gitSource, pathFileName, slugFileName }) => {
 
     const isGitCommit = isGitAdd && shell.exec(`git commit -m ${slugFileName}`, shellOption).code === 0
     setStatus(isGitCommit, 'isGitCommit')
-    const isGitPush = isGitCommit && shell.exec(`eval "$(ssh-agent -s)"
-                        ssh-add ~/.ssh/nhuyk56
-                        ssh -T git@github.com
-                        echo '<------------------------------------------------------------------------------------------------------------>'
-                        git push -u origin ${brandMD5}
-                        echo '<------------------------------------------------------------------------------------------------------------>'
-                        `, shellOption).code === 0
+    const isGitPush = isGitCommit && shell.exec(`git push -u origin ${brandMD5}`, shellOption).code === 0
     setStatus(isGitPush, 'isGitPush')
     isCurrentCorrectBrand = (isGitPush || gitAddBlank) && shell.exec('git status', shellOption).stdout.includes(`On branch ${brandMD5}`)
     const hasValidateFile =  shell.exec(`git checkout index.m3u8`, shellOption).code === 0
